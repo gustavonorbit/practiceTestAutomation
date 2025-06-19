a6c7d4d6ef7679123559199bfb29ca88ac5cb38f
Gustavo Norberto <gustavobarros12654@gmail.com>
2025-06-19 01:07:18 -0300

    ConfigProject

--- AutomationTest-1.py
+++ AutomationTest-1.py
@@ -1,4 +1,18 @@
 from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = Chrome()
+#Caminho para o webdriver
+path = "C:\\Users\\Home\\Desktop\\newProjectAutomationTest\\chromedriver-win64\\chromedriver.exe"
+
+# Criando a instância de Service com o caminho do chromedriver
+service = Service(path)
+
+# Definindo as opções do Chrome
+options = webdriver.ChromeOptions()
+
+# Inicializando o driver com o Service e as opções
+driver = webdriver.Chrome(service=service, options=options)
+
+# Abrir Chrome
 driver.get("https://thetestingworld.com/testings/")
