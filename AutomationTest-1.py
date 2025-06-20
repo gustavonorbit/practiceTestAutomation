d3626b35341afa00705858c63327d7e4ff69d4df
Gustavo Norberto <gustavobarros12654@gmail.com>
2025-06-20 18:53:01 -0300

    New features

--- AutomationTest-1.py
+++ AutomationTest-1.py
@@ -1,18 +1,39 @@
-from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
-#Caminho para o webdriver
+#Web driver path
 path = "C:\\Users\\Home\\Desktop\\newProjectAutomationTest\\chromedriver-win64\\chromedriver.exe"
 
-# Criando a instância de Service com o caminho do chromedriver
+
+# Crete service instance with path of chromedriver
 service = Service(path)
 
-# Definindo as opções do Chrome
+# Define chrome options
 options = webdriver.ChromeOptions()
 
-# Inicializando o driver com o Service e as opções
+# Driver inicializing with service and options
 driver = webdriver.Chrome(service=service, options=options)
 
-# Abrir Chrome
+# Open Chrome and go to test website
 driver.get("https://thetestingworld.com/testings/")
+
+#Interact with window of browser
+driver.maximize_window()
+
+#Enter Data to the textbox
+driver.find_element(By.NAME,"fld_username").send_keys("testerQA")
+driver.find_element(By.XPATH,"//input[@name='fld_email']").send_keys("testerQAemail@gmail.com")
+
+#Click on radio button
+driver.find_element(By.XPATH,"//input[@value='home']").click()
+
+#Select option on Check Box
+select_element = driver.find_element(By.NAME, "sex")
+select = Select(select_element)
+select.select_by_value("2")
+
+#Time of 3 second
+time.sleep(5)
