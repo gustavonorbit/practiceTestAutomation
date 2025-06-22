f184e480b38aebff649d0a686fd08fd479aac938
Gustavo Norberto <gustavobarros12654@gmail.com>
2025-06-21 21:04:25 -0300

    Completed Login tests

--- AutomationTest-1.py
+++ AutomationTest-1.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 #Web driver path
@@ -26,14 +27,45 @@ driver.maximize_window()
 #Enter Data to the textbox
 driver.find_element(By.NAME,"fld_username").send_keys("testerQA")
 driver.find_element(By.XPATH,"//input[@name='fld_email']").send_keys("testerQAemail@gmail.com")
+driver.find_element(By.NAME,"fld_password").send_keys("pass123@")
+driver.find_element(By.NAME,"fld_cpassword").send_keys("pass123@")
+driver.find_element(By.NAME,"dob").send_keys("19/11/1997")
+driver.find_element(By.NAME,"terms").click()
+driver.find_element(By.NAME,"phone").send_keys("61991074681")
+driver.find_element(By.NAME,"address").send_keys("qr 8 cj J cs 14 Setor Sul")
+
+#Time for wait load page
+time.sleep(2)
 
 #Click on radio button
 driver.find_element(By.XPATH,"//input[@value='home']").click()
 
-#Select option on Check Box
-select_element = driver.find_element(By.NAME, "sex")
-select = Select(select_element)
+#Select option 1
+select_element1 = driver.find_element(By.NAME, "sex")
+select = Select(select_element1)
 select.select_by_value("2")
+#Select option 2 
+select_element2 = driver.find_element(By.NAME,"country")
+select2 = Select(select_element2)
+select2.select_by_value("30")
+#TIME OF 3 SECONDS
+time.sleep(5)
+#Select option 3
+select_element3 = driver.find_element(By.NAME,"state")
+select3 = Select(select_element3)
+select3.select_by_value("514")
+#Time of 3 Seconds
+time.sleep(5)
+#Select option 4
+select_element4 = driver.find_element(By.NAME,"city")
+select4 = Select(select_element4)
+select4.select_by_value("8597")
 
-#Time of 3 second
+#Enter to zipcode
+driver.find_element(By.NAME,"zip").send_keys("72415410")
+
+#click to submmit
+driver.find_element(By.XPATH,"//input[@value='Sign up']").click()
+
+#Time of 5 seconds for look results
 time.sleep(5)
