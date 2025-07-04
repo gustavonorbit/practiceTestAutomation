e44839d30814ceefe9de9a08797cc032639936eb
Gustavo Norberto <gustavobarros12654@gmail.com>
2025-07-03 22:55:06 -0300

    Update_newFeature_handlePage

--- handle_multiple_popups.py
+++ handle_multiple_popups.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
+
 path = "D:\\newProjectAutomationTest - Selenium\\chromedriver-win64\\chromedriver.exe"
 service = Service(path)
 drive = Chrome(service=service)
@@ -10,6 +12,10 @@ Allwindow = drive.window_handles
 for win in Allwindow:
     drive.switch_to.window(win)
     if(drive.current_url=="https://demo.guru99.com/articles_popup.php"):
+        drive.find_element(By.NAME,"emailid").send_keys("teste@email.com")
+        drive.find_element(By.NAME,'btnLogin').click()
+        
         print("This is my main window")
     else: 
         drive.close()
+assert "Access details to demo site" in drive.page_source, "Texto não encontrado"
